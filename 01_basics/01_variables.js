d6a446aa4bdecf5a681510fdacb6f2f541805031
Mohaimen Hridoy <mohaimenhridoy@gmail.com>
2026-03-17 03:41:25 +0600

    Variables added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId=600002;
+let accountEmail="mohaimenhriody@gmail.com";
+var accountPassword="12345";
+accountCity="Kishoreganj";
+
+// accountId=2; // not allowed
+
+console.log(accountId);
+
+/*Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+let accountState;
+
+accountEmail="mohaimenhridoy275@gmail.com"
+
+accountPassword="234242"
+
+accountCity="Dhaka"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
